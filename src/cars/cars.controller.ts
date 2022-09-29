b8b160eb5239308ab5948b6789738c37621e2631
FelipeHernandez98 <andresfelipehc@ufps.edu.co>
2022-09-28 23:18:58 -0500

    creando ruta get para pedir carro por id

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
 
+    private cars = ['Toyota', 'Chevrolet', 'Renault'];
+
     @Get()
     getAllCars(){
-        return ['Toyota', 'Chevrolet', 'Renault'];
+        return this.cars;
+    }
+
+    @Get(':id')
+    getCarById( @Param('id') id: string ){
+        return this.cars[id];
     }
 }
+
